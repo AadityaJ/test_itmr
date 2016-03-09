c66809128d50b1feef13a738d3b14cfc0d41ecd2
AadityaJ <aadityajamuar@gmail.com>
2016-03-09 18:49:55 +0530

    2 done compeletely

--- 2.R
+++ 2.R
@@ -1,20 +1,34 @@
 library(NLP)
 library(openNLP)
-tagPOS <-  function(x, ...) {
-  s <- as.String(x)
+##uses part-of-speech modules of openNLP
+##useful link :: http://www.martinschweinberger.de/blog/part-of-speech-tagging-with-r/
+##for all of the division
+
+tagPOS <-  function(dat, ...) {
+  st <- as.String(dat)
+  ##using functions from Apache openNLP
+  ##for documentation and working https://opennlp.apache.org/
   word_token_annotator <- Maxent_Word_Token_Annotator()
-  a2 <- Annotation(1L, "sentence", 1L, nchar(s))
-  a2 <- annotate(s, word_token_annotator, a2)
-  a3 <- annotate(s, Maxent_POS_Tag_Annotator(), a2)
+  
+  
+  a2 <- Annotation(1L, "sentence", 1L, nchar(st))
+  
+  a2 <- annotate(st, word_token_annotator, a2)
+  
+  a3 <- annotate(st, Maxent_POS_Tag_Annotator(), a2)
+  ## "word"
   a3w <- a3[a3$type == "word"]
   POStags <- unlist(lapply(a3w$features, `[[`, "POS"))
-  POStagged <- paste(sprintf("%s/%s", s[a3w], POStags), collapse = " ")
+  
+  POStagged <- paste(sprintf("%s/%s", st[a3w], POStags), collapse = " ")
+  ##return list
   list(POStagged = POStagged, POStags = POStags)
 }
 
-
+##to test
 
 str <- "this is a the first sentence."
 tagged_str <-  tagPOS(str)
 
-sapply(strsplit(txtTag,"[[:punct:]]*/VB.?"),function(x) sub("(^.*\\s)(\\w+$)", "\\2", x))
+
+## end
--- 3.R
+++ 3.R
@@ -1,2 +1,5 @@
+##finding out all the verbs as VB
+
+
 tagged.text_2 <- treetag("~/Desktop/test_itmr/text.txt", treetagger="manual",
                        lang="en", TT.options=NULL,file = )
