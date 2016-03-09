63608a69c5dfec3a2ee75b19faf497a1420acf99
AadityaJ <aadityajamuar@gmail.com>
2016-03-09 19:13:33 +0530

    adding 3

--- 3.R
+++ 3.R
@@ -1,5 +1,13 @@
 ##finding out all the verbs as VB
-
-
-tagged.text_2 <- treetag("~/Desktop/test_itmr/text.txt", treetagger="manual",
-                       lang="en", TT.options=NULL,file = )
+##uses inbuilt stringr library
+##no use of sneaky one line functions . Making the code as readable as posssible 
+##verb is VB source :: http://www.martinschweinberger.de/blog/part-of-speech-tagging-with-r/
+library(stringr)
+print_verb<-function(txtTag){
+  ##word(strsplit(txtTag$POStagged,"/VB")[[1]][6],-1)
+  
+  list<-word(strsplit(txtTag$POStagged,"/VB")[[1]],-1)
+  ##list has the set of verbs now
+  ##last word is vestigal . Removing it
+  list[1:(length(list)-1)]
+}
